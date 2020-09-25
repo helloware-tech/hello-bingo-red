f2affb51105278e140123461438dfa3ea2acb1f4
Roberto Quintero <rcql7275@gmail.com>
2020-09-25 10:13:31 -0500

    🐛 FIX: Bug in App.component.ts fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
-import { SwUpdate } from '@angular/service-worker';
 
-import { MenuController, ToastController } from '@ionic/angular';
-import { Toast } from './functions/toast';
+import { MenuController } from '@ionic/angular';
+import { Tools } from './functions';
 import { MyRoute } from './interfaces/my-route';
 import { AuthService } from './providers/auth.service';
 import { RoutesService } from './providers/routes.service';
@@ -14,7 +13,7 @@ import { RoutesService } from './providers/routes.service';
 	encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent extends Toast implements OnInit {
+export class AppComponent extends Tools implements OnInit {
 	public pages: Array<MyRoute> = [];
 	loggedIn = false;
 	dark = false;
@@ -22,8 +21,6 @@ export class AppComponent extends Toast implements OnInit {
 	constructor(
 		private menu: MenuController,
 		private router: Router,
-		private swUpdate: SwUpdate,
-		private toastCtrl: ToastController,
 		private routes: RoutesService,
 		private auth: AuthService
 	) {
@@ -33,36 +30,15 @@ export class AppComponent extends Toast implements OnInit {
 
 	async ngOnInit() {
 		this.menu.enable(true);
-
-		this.swUpdate.available.subscribe(async res => {
-			const toast = await this.toastCtrl.create({
-				message: 'Update available!',
-				position: 'bottom',
-				buttons: [
-					{
-						role: 'cancel',
-						text: 'Reload'
-					}
-				]
-			});
-
-			await toast.present();
-
-			toast
-				.onDidDismiss()
-				.then(() => this.swUpdate.activateUpdate())
-				.then(() => window.location.reload());
-		});
 	}
 
 	initializeApp() {
 		this.pages = this.routes.primaries;
-		console.log('Inicializaded');
 	}
 
 	logout() {
 		this.auth.logout().then((message) => {
-			this.presentToast(message);
+			this.toast.present(message);
 			return this.router.navigateByUrl('/login');
 		});
 	}
